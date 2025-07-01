9eb5a5dba2aa29d00d8852966bee2689c77c1aa0
Noel Pinto <noelpinto47@gmail.com>
2025-07-01 23:27:29 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { Button } from "@mui/material";
 
 function App() {
   return (
     <div>
       <h1>Hello, React!</h1>
+      <Button variant="contained">Hello world</Button>
     </div>
   );
 }
